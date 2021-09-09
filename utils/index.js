c07bfd986e0ad028780f8de033504551b8c1151d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-08 23:01:51 -0700

    feat(implement-newsapi): create a filter object function

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
 const AppError = require('./appError');
 const catchAsync = require('./catchAsync');
+const filterObj = require('./filterObject');
 const sanitize = require('./sanitize');
 
-module.exports = { AppError, catchAsync, sanitize };
+module.exports = { AppError, catchAsync, filterObj, sanitize };
